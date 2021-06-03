59a2e8ee7a94c2666c1a64a7f73180886d96b65f
rudragthite <rudragthite@gmail.com>
2021-06-03 15:08:18 +0530

    Subtract implementation with ERROR code

--- math.py
+++ math.py
@@ -3,7 +3,10 @@ def add(x,y):
     return x+y
 #sub implementation
 def subtract(x,y):
-    pass
+    if y>x:
+        return ERROR
+    else:
+        return x-y
 #multiply implementation
 def multiply(x,y):
     pass
